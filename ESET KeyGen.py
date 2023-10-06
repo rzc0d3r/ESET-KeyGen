a3cae510c71e2b20c435a0248140c20615f02ad8
Artem <106631540+rzc0d3r@users.noreply.github.com>
2023-10-06 16:04:42 +0300

    Update to version 1.0.7 (061023-1542)
    1. Added automatic and chrome driver update and installation

--- ESET KeyGen.py
+++ ESET KeyGen.py
@@ -1,17 +1,20 @@
-# Version 1.0.6.2a (01.10.2023)
-import re
+# Version 1.0.7 (061023-1542)
+VERSION = 'v1.0.7 (061023-1542) by rzc0d3r'
+import modules.chrome_driver_installer as chrome_driver_installer
+
+import datetime
+import requests
 import time
+import os
+import re
 
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.chrome.service import Service
 
+from subprocess import check_output, PIPE
 from string import ascii_letters
 from random import choice
-
-import datetime
-import requests
-import os
 
 GET_EBCN = 'document.getElementsByClassName'
 GET_EBID = 'document.getElementById'
@@ -216,6 +219,47 @@ class EsetKeygen:
         
 if __name__ == '__main__':
     try:
+        # auto updating or installing chrome driver
+        print('-- Chrome Driver Auto-Installer {0} --\n'.format(chrome_driver_installer.VERSION))
+        chrome_version, _, chrome_major_version, _, _ = chrome_driver_installer.get_chrome_version()
+        if chrome_version is None:
+            print('[-] Chrome is not detected on your device!')
+            raise RuntimeError
+        current_chromedriver_version = None
+        platform, arch = chrome_driver_installer.get_platform_for_chrome_driver()
+        chromedriver_name = 'chromedriver.exe'
+        if platform != 'win':
+            chromedriver_name = 'chromedriver'
+        if os.path.exists(chromedriver_name):
+            os.chmod(chromedriver_name, 0o777)
+            out = check_output([chromedriver_name, "--version"], stderr=PIPE)
+            if out is not None:
+                current_chromedriver_version = out.decode("utf-8").split(' ')[1]
+        print('[*] Chrome version: {0}'.format(chrome_version))
+        print('[*] Chrome driver version: {0}'.format(current_chromedriver_version))
+        if current_chromedriver_version is None:
+            print('\n[-] Chrome driver not detected, download attempt...')
+        elif current_chromedriver_version.split('.')[0] != chrome_version.split('.')[0]: # major version match
+            print('\n[-] Chrome driver version doesn\'t match version of the installed chrome, trying to update...')
+        if current_chromedriver_version is None or current_chromedriver_version.split('.')[0] != chrome_version.split('.')[0]:
+            driver_url = chrome_driver_installer.get_driver_download_url()
+            if driver_url is None:
+                print('\n[-] Couldn\'t find the right version for your system!')
+                method = input('\nRun the program anyway? (y/n): ')
+                if method == 'n':
+                    exit(-1)
+            else:
+                print('\n[+] Found a suitable version for your system!')
+                print('\n[*] Download attempt...')
+                if chrome_driver_installer.download_chrome_driver('.', driver_url):
+                    print('[+] The Сhrome driver was successfully downloaded and unzipped!')
+                    input('\nPress Enter to continue...')
+                else:
+                    print('[-] Error downloading or unpacking!')
+                    method = input('\nRun the program anyway? (y/n): ')
+                    if method == 'n':
+                        exit(-1)
+        print('\n-- ESET KeyGen {0} --\n'.format(VERSION))
         email_obj = Email()
         email_obj.register()
         eset_password = SharedTools.createPassword(6)
