a14a750942fbb5496c88108d5c99fef03b020a24
Artem <106631540+rzc0d3r@users.noreply.github.com>
2025-06-24 17:54:51 +0300

    Update to v1.5.5.5

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if ('--disable-logging' not in sys.argv and not MBCI_MODE) or ('--disable-loggin
 from modules.EmailAPIs import *
 
 # ---- Quick settings [for Developers to quickly change behavior without changing all files] ----
-VERSION = ['v1.5.5.4', 1554]
+VERSION = ['v1.5.5.5', 1555]
 LOGO = f"""
 ███████╗███████╗███████╗████████╗   ██╗  ██╗███████╗██╗   ██╗ ██████╗ ███████╗███╗   ██╗
 ██╔════╝██╔════╝██╔════╝╚══██╔══╝   ██║ ██╔╝██╔════╝╚██╗ ██╔╝██╔════╝ ██╔════╝████╗  ██║
